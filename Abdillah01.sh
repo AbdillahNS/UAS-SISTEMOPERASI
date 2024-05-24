29ba4161a098af55bf6f06ad39397f7946c01653
AbdillahNS <abdillahnoersaid@gmail.com>
2024-05-24 09:06:24 +0700

    Program

---
+++ Abdillah01.sh
@@ -0,0 +1,56 @@
+!/bin/bash
+
+# Nama : Abdillah Noer Said
+# NIM : 2341720018
+
+function lihatFile() {
+    echo "File dalam direktori:"
+    ls
+}
+
+function tambahFile() {
+    echo -n "Masukkan nama file yang ingin Anda buat:"
+    read nama_file
+    touch "$nama_file"
+    echo "File \"$nama_file\" berhasil dibuat."
+}
+
+function hapusFile() {
+    echo -n "Masukkan nama file yang ingin dihapus: "
+    read nama_file
+    
+    if [ -f "$nama_file" ]; then
+        rm -f "$nama_file"
+        echo "File '$nama_file' telah dihapus."
+    else
+        echo "File '$nama_file' tidak ditemukan."
+    fi
+}
+
+function downloadManager() {
+    echo -n "Masukkan URL file yang ingin diunduh: "
+    read url
+    wget -c "$url"
+}
+
+while true; do
+    echo "-----------------------------------------"
+    echo "Main Menu:"
+    echo "1. Lihat file"
+    echo "2. Tambah file"
+    echo "3. Hapus file"
+    echo "4. Downlaod Manager"
+    echo "0. Keluar"
+    echo "-----------------------------------------"
+
+    read -p "Masukkan Pilihan: " menu
+
+    case $menu in
+        1) lihatFile ;;
+        2) tambahFile ;; 
+        3) hapusFile ;;
+        4) downloadManager ;;
+        0) exit 0 ;;
+        *) echo "Menu yang Anda masukkan tidak ada" ;;
+    esac
+done
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# UAS-SISTEMOPERASI
+# Data Diri
+
+|  |  |
+|--|--|
+| NIM | **2341720018** |
+| Nama Lengkap | **Abdillah Noer Said** |
+| Kelas | TI-1B |
+| Dosen Pengampu | Mohammad Faried Rahmat, S.ST., M.Tr.T dan Erfan Rohadi, ST., M.Eng., Ph.D|
